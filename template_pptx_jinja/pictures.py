24590ab8f75e27b712f61aac46e5aca6aeeff8e7
王录祥 <wanglx@smartsteps.com>
2023-05-22 14:07:29 +0800

    修复图片渲染

--- template_pptx_jinja/pictures.py
+++ template_pptx_jinja/pictures.py
@@ -12,13 +12,12 @@ def get_hash(filename):
     return sha1.hexdigest()
 
 # See https://github.com/scanny/python-pptx/issues/116
-def replace_img_slide1(slide, img, img_path):
+def replace_img_slide(slide, img, img_path):
     # Replace the picture in the shape object (img) with the image in img_path.
     
     imgPic = img._pic
     imgRID = imgPic.xpath('./p:blipFill/a:blip/@r:embed')[0]
     imgPart = slide.part.related_part(imgRID)
-    #imgPart = slide.part.get_image(imgRID)
 
     with open(img_path, 'rb') as f:
         rImgBlob = f.read()
