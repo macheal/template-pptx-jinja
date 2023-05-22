3a2cb0cb531c01f83eb2905edd0ae2fb970c14ce
王录祥 <wanglx@smartsteps.com>
2023-05-22 11:57:49 +0800

     修复升级python-pptx到 0.6.21之后图片无法渲染的问题

--- template_pptx_jinja/pictures.py
+++ template_pptx_jinja/pictures.py
@@ -2,6 +2,7 @@ import hashlib
 
 
 from PIL import Image
+from pptx.util import Inches
 
 
 def get_hash(filename):
@@ -11,12 +12,12 @@ def get_hash(filename):
     return sha1.hexdigest()
 
 # See https://github.com/scanny/python-pptx/issues/116
-def replace_img_slide(slide, img, img_path):
+def replace_img_slide1(slide, img, img_path):
     # Replace the picture in the shape object (img) with the image in img_path.
-
+    
     imgPic = img._pic
     imgRID = imgPic.xpath('./p:blipFill/a:blip/@r:embed')[0]
-    imgPart = slide.part.related_parts[imgRID]
+    imgPart = slide.part.related_part(imgRID)
     #imgPart = slide.part.get_image(imgRID)
 
     with open(img_path, 'rb') as f:
@@ -24,3 +25,6 @@ def replace_img_slide(slide, img, img_path):
 
     # replace
     imgPart._blob = rImgBlob
+
+
+
